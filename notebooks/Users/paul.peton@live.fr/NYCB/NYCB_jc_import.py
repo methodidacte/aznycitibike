cbf7287238bb5f86109f390c7bf945b7e4b24aeb
methodidacte <paul.peton@live.fr>
2019-11-01 20:15:56 +0100

    New York citibike (dataprep)

---
+++ notebooks/Users/paul.peton@live.fr/NYCB/NYCB_jc_import.py
@@ -0,0 +1,53 @@
+# Databricks notebook source
+param_dev = dbutils.widgets.get("wEnv")
+
+# COMMAND ----------
+
+df_jc = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load("/mnt/"+param_dev+"/JC*")
+
+# COMMAND ----------
+
+print(df_jc.count())
+
+# COMMAND ----------
+
+df_jc = df_jc.withColumnRenamed('Trip Duration', 'tripduration')
+df_jc = df_jc.withColumnRenamed('start time', 'starttime')
+df_jc = df_jc.withColumnRenamed('stop time', 'stoptime')
+df_jc = df_jc.withColumnRenamed('start station id', 'startstationid')
+df_jc = df_jc.withColumnRenamed('start station name', 'startstationname')
+df_jc = df_jc.withColumnRenamed('start station longitude', 'startstationlongitude')
+df_jc = df_jc.withColumnRenamed('start station latitude', 'startstationlatitude')
+df_jc = df_jc.withColumnRenamed('end station id', 'endstationid')
+df_jc = df_jc.withColumnRenamed('end station name', 'endstationname')
+df_jc = df_jc.withColumnRenamed('end station longitude', 'endstationlongitude')
+df_jc = df_jc.withColumnRenamed('end station latitude', 'endstationlatitude')
+df_jc = df_jc.withColumnRenamed('bike id', 'bikeid')
+df_jc = df_jc.withColumnRenamed('user type', 'usertype')
+df_jc = df_jc.withColumnRenamed('birth year', 'birthyear')
+
+# COMMAND ----------
+
+df_jc.write.mode('overwrite').format("parquet").saveAsTable("jc_citibike")
+
+# COMMAND ----------
+
+# %run path/to/NotebookB $VarA="ValueA" $VarB="ValueB"
+
+# COMMAND ----------
+
+import json
+
+dbutils.notebook.exit(
+  json.dumps({
+    "status": "OK",
+    "table": "jc_citibike"
+  })
+)
+
+# COMMAND ----------
+
+dbutils.notebook.exit("end of data collection (initial notebook)")
+
+# COMMAND ----------
+
